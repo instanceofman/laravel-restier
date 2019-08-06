c11bfab03f9d9d87dac5a03c55c86d88a6536f5a
Nam Nguyen <namnd.hn@gmail.com>
2019-08-06 14:40:40 +0700

    Add post json feature

--- src/Restier.php
+++ src/Restier.php
@@ -7,63 +7,72 @@ class Restier implements HttpMethods
 {
     const DEFAULT_OPTIONS = ['verify' => false, 'timeout' => 60];
 
-    public function get(string $url, $headers = [], $options = []) : Package
+    public function get(string $url, $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::get($url, $headers, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function post(string $url, $data = [], $headers = [], $options = []) : Package
+    public function post(string $url, $data = [], $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::post($url, $headers, $data, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function delete(string $url, $headers = [], $options = []) : Package
+    public function delete(string $url, $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::delete($url, $headers, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function head(string $url, $headers = [], $options = []) : Package
+    public function head(string $url, $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::head($url, $headers, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function trace(string $url, $headers = [], $options = []) : Package
+    public function trace(string $url, $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::trace($url, $headers, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function put(string $url, $data = [], $headers = [], $options = []) : Package
+    public function put(string $url, $data = [], $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::put($url, $headers, $data, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function options(string $url, $data = [], $headers = [], $options = []) : Package
+    public function options(string $url, $data = [], $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::options($url, $headers, $data, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    public function patch(string $url, $data = [], $headers = [], $options = []) : Package
+    public function patch(string $url, $data = [], $headers = [], $options = []): Package
     {
         return $this->resolveRequest(
             \Requests::patch($url, $headers, $data, $this->resolveOptions($options)), func_get_args()
         );
     }
 
-    protected function resolveRequest(\Requests_Response $request, $args) : Package
+    public function postJson(string $url, $data = [], $headers = [], $options = []): Package
+    {
+        return $this->post($url,
+            json_encode($data),
+            array_merge($headers, ['Content-Type' => 'application/json']),
+            $options
+        );
+    }
+
+    protected function resolveRequest(\Requests_Response $request, $args): Package
     {
         return (new Package($request))->attachPayload($args['data'] ?? []);
     }
